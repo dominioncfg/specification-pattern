eee821a4f01048c003ad318210a88a6a3a986654
Jose Carlos <josecdom94@gmail.com>
2025-12-22 12:03:48 +0100

    Refactor Ef Apply

--- SpecificationExample.Host/Infra/Extensions/SpecificationApplyExtensions.cs
+++ SpecificationExample.Host/Infra/Extensions/SpecificationApplyExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
 using SpecificationExample.Domain.Common;
 using System.Linq.Expressions;
 
@@ -15,44 +14,81 @@ public static class SpecificationApplyExtensions
 
     private static IQueryable<T> ApplySpecificationWithEFCore<T>(this IQueryable<T> query, Specification<T> specification) where T : Entity
     {
-        if (specification.QueryTags.Any())
+        return query
+             .ApplyQueryTags(specification)
+             .ApplyIncludes(specification)
+             .ApplyAsNoTracking(specification)
+             .ApplyAsSplitQuery(specification)
+             .Filter(specification);
+    }
+
+    private static IQueryable<T> ApplyQueryTags<T>(this IQueryable<T> query, Specification<T>? specification) where T : Entity
+    {
+        if (specification is null)
+            return query;
+
+        if (specification.QueryTags is null || !specification.QueryTags.Any())
+            return query;
+
+        foreach (var tag in specification.QueryTags)
         {
-            //TODO: in EF Core Namespace
-            foreach (var tag in specification.QueryTags)
-            {
-                query = query.TagWith(tag);
-            }
+            query = query.TagWith(tag);
         }
 
-        if (specification.Includes is not null && specification.Includes.Count > 0)
+        return query;
+    }
+
+    private static IQueryable<T> ApplyIncludes<T>(this IQueryable<T> query, Specification<T>? specification) where T : Entity
+    {
+        if (specification is null)
+            return query;
+
+        if (specification.Includes is null || specification.Includes.Count == 0)
+            return query;
+
+        for (int i = 0; i < specification.Includes.Count; i++)
         {
-            for (int i = 0; i < specification.Includes.Count; i++)
-            {
-                Expression<Func<T, object>>? include = specification.Includes[i];
-                if (i == 0)
-                {
-                    //TODO: in EF Core Namespace
-                    query = query.Include(include);
-                }
-                else
-                {
-                    //TODO: in EF Core Namespace
-                    query = ((IIncludableQueryable<T, object>)query).Include(include);
-                }
-            }
+            Expression<Func<T, object>>? include = specification.Includes[i];
+            query = query.Include(specification.Includes[0]);
         }
 
-        if (specification.AsNoTracking)
+        return query;
+    }
+
+    private static IQueryable<T> ApplyAsNoTracking<T>(this IQueryable<T> query, Specification<T>? specification) where T : Entity
+    {
+        if (specification is null)
+            return query;
+
+        if (!specification.AsNoTracking.HasValue)
+            return query;
+
+        if (specification.AsNoTracking.Value)
         {
-            //TODO: in EF Core Namespace
             query = query.AsNoTracking();
         }
 
-        if (specification.SplitQuery)
+        if (!specification.AsNoTracking.Value)
+        {
+            query = query.AsTracking();
+        }
+
+        return query;
+    }
+
+    private static IQueryable<T> ApplyAsSplitQuery<T>(this IQueryable<T> query, Specification<T>? specification) where T : Entity
+    {
+        if (specification is null)
+            return query;
+
+        if (!specification.SplitQuery.HasValue)
+            return query;
+
+        if (specification.SplitQuery.Value)
         {
             query = query.AsSplitQuery();
         }
-    
-        return query.Filter(specification);
-    }  
+
+        return query;
+    }
 }
